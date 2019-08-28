3ef3b634f8f6b4f4ffdca07a663a1bc01990bc91
Mantas Kemesius <43745867+mantaskemesius@users.noreply.github.com>
2019-08-28 08:22:32 +0300

    open page with right url after launch

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const http = require("http");
 const httpProxy = require("http-proxy");
-
-const target = "localhost:3000";
+const opn = require('opn');
+const ip = require("ip");
+const target = "http://localhost:3000";
 
 const proxy = httpProxy.createProxyServer({});
+const PORT = 5050;
 
 proxy.on("proxyReq", function(proxyReq, req, res, options) {});
 
 http
   .createServer(function(req, res) {
-    console.log('Request host:', req.headers.host);
-    console.log('Request UserAgent:', req.headers['user-agent']);
-    console.log('------------------');
     proxy.web(req, res, {
       target,
       changeOrigin: true
     });
   })
   .listen(5050, () => {
-    console.log("Waiting for requests...");
+    const url = `http://${ip.address()}:${PORT}`;
+    console.log("URL:", url);
+    opn(url);
   });
